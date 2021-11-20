c1785a0ff83b6ab051082e9dbac0110c7acf1c17
Peter Stroessler <7starch@gmail.com>
2021-11-20 16:32:06 +0100

    add of https switch server uri

--- src/components/Config.ts
+++ src/components/Config.ts
@@ -1,3 +1,11 @@
-export const REST_URI = "http://ps007server.herokuapp.com/api/projects";
-
-//export const REST_URI = "http://localhost:5000/api/projects";
+const href = window.location.href;
+//non https
+let resturi = "http://ps007server.herokuapp.com/api/projects";
+//https
+if (href.indexOf("https://keepitnative.herokuapp.com") > -1) {
+    resturi = "https://ps007server.herokuapp.com/api/projects";
+} else if (href.indexOf("http://localhost:3000") > -1) {
+    //local testing
+    resturi = "http://localhost:5000/api/projects";
+}
+export const REST_URI = resturi;
